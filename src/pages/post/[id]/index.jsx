f4d044336240dea762fa91f14a2dcabe5b38a6d7
o-noji <okaden.denden@gmail.com>
2022-08-07 23:44:46 +0900

    ✨ ユーザーのfetch,fetcherの共有化。

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -1,18 +1,12 @@
-import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/componets/Header";
-import { useRouter } from "next/router";
+import { Post } from "src/componets/Post";
 
 const PostID = () => {
-  const router = useRouter();
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   );
 };
