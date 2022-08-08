7eb33fa1126e80e857754cb17bf91531eca23436
o-noji <okaden.denden@gmail.com>
2022-08-09 00:07:32 +0900

    🎨 Layoutをコンポーネントを使ってまとめた。

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,10 +1,9 @@
-import styles from "src/styles/Home.module.css";
 import { Header } from "src/componets/Header";
 import { Post } from "src/componets/Post";
 
 const PostsId = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
       <Post />
     </div>
--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import styles from "src/styles/Home.module.css";
 import { Header } from "src/componets/Header";
 import { Posts as PostsCompornent } from "src/componets/Posts";
 
 const Posts = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Posts Page</title>
       </Head>
