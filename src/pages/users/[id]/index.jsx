199b37db70c6c5a28106e343797066677dd35b4c
o-noji <okaden.denden@gmail.com>
2022-09-11 00:43:21 +0900

    ✨ ユーザー詳細ページでSWRを実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,12 +1,29 @@
 import { Header } from "src/componets/Header";
 import { UsersCompornent } from "src/componets/User";
+import { SWRConfig } from "swr";
 
-const UsersId = () => {
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UsersId = (props) => {
+  const { fallback } = props;
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <UsersCompornent />
-    </div>
+    </SWRConfig>
   );
 };
 
